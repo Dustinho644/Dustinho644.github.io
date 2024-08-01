a39936200b3b17f71e9321bbbd0de49eb74472f3
Dustinho664 <Ho.dustin.supperdd@gmail.com>
2024-08-01 11:47:42 -0700

    hh

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-prompt("hi ")
+alert("welcom to rock paper scissors")
+const rockButton = document.getElementById("rockButton")
+const paperButton = document.getElementById ("paperButton")
+const scissorButton = document.getElementById("scissorButton")
+const playerButtonthing = document.getElementById("playerButtonthing")
+const randomInteger = Math.floor(Math.random() * 3) + 1;
+
+
+playerButtonthing.addEventListener ("click", computerBrain) 
+
+function computerBrain(){
+    if (randomInteger === 1) {
+        document.getElementById("computerBrain").textContent = "rock" 
+    }
+    else if (randomInteger === 2) {
+        document.getElementById("computerBrain").textContent = "paper"
+    }
+    else {
+        document.getElementById("computerBrain").textContent = "scissors"
+    }
+    }
