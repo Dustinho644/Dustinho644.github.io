51f89ad2f04f7316eed2cea092073931b8b30992
Dustinho664 <Ho.dustin.supperdd@gmail.com>
2024-08-02 10:12:12 -0700

    hu

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 alert("welcom to rock paper scissors")
-const rockButton = document.getElementById("rockButton")
-const paperButton = document.getElementById ("paperButton")
-const scissorButton = document.getElementById("scissorButton")
-const playerButtonthing = document.getElementById("playerButtonthing")
+const rockButton = document.getElementById("rockButton");
+const paperButton = document.getElementById ("paperButton");
+const scissorButton = document.getElementById("scissorButton");
+const playerButtonthing = document.getElementById("playerButtonthing");
 
-let userChoice
-let randomInteger
+let userChoice;
+let randomInteger;
+let points = 0;
 
 rockButton.addEventListener ("click", function () {
     userChoice = 1;
@@ -45,41 +46,46 @@ function compare(){
     else if ((userChoice === 1) && (randomInteger === 2)) {
         document.getElementById("messageThing").textContent = "good try PERSON"
         document.body.style.backgroundColor="red"
+        points--
     }
    
     else if ((userChoice === 1) && (randomInteger === 3)) {
         document.getElementById("messageThing").textContent = "you win... Is that good?"
         document.body.style.backgroundColor="green"
+        points++
     }
 
-    if ((userChoice === 2) && (randomInteger === 1)) {
+    else if ((userChoice === 2) && (randomInteger === 1)) {
         document.getElementById("messageThing").textContent = "OMG wow:)"
         document.body.style.backgroundColor="green"
-
+        points++
     }
 
-    if ((userChoice === 2) && (randomInteger === 3)) {
+    else if ((userChoice === 2) && (randomInteger === 3)) {
         document.getElementById("messageThing").textContent = "awww. you lost:("
         document.body.style.backgroundColor="red"
-
-
+        points--
     }
 
-    if ((userChoice === 3) && (randomInteger === 1)) {
+    else if ((userChoice === 3) && (randomInteger === 1)) {
         document.getElementById("messageThing").textContent = "you lost... is that bad?"
         document.body.style.backgroundColor="red"
+        points--
     }
 
-    if ((userChoice === 3) && (randomInteger === 2)) {
-        document.getElementById("messageThing").textContent = "Sooo... ummmmm...good job I guss"
-        document.body.style.backgroundColor="green"
-
+    else {
+        document.getElementById("messageThing").textContent = "Sooo... ummmmm...good job I guss";
+        document.body.style.backgroundColor="green";
+        points++
     }
 }
 
 function displayMessages(){
+    // attempts++
     computerBrain()
     compare()
     console.log(userChoice)
     console.log(randomInteger)
+    document.getElementById("displayPoints").textContent = `${points}`
 }
+
